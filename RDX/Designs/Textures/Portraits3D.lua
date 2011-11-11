46a73a3d09ed1461bb923713a041f7c7c2239f69
Sigg <sigg@wowrdx.com>
2011-11-11 09:27:43 +0100

    add new portrait 3D

--- RDX/Designs/Textures/Portraits3D.lua
+++ RDX/Designs/Textures/Portraits3D.lua
@@ -149,3 +149,141 @@ end
 		};
 	end;
 });
+
+local units = {
+	{ text = "player" },
+	{ text = "target" },
+	{ text = "focus" },
+	{ text = "pet" },
+};
+local function unitSel() return units; end
+
+RDX.RegisterFeature({
+	name = "portrait_3d2"; version = 1; multiple = true;
+	title = VFLI.i18n("Blizzard New 3D Portrait");
+	category = VFLI.i18n("Complexes");
+	IsPossible = function(state)
+		if not state:Slot("DesignFrame") then return nil; end
+		if not state:Slot("Base") then return nil; end
+		return true;
+	end;
+	ExposeFeature = function(desc, state, errs)
+		if not RDXUI.DescriptorCheck(desc, state, errs) then return nil; end
+		if desc.owner == "Base" then desc.owner = "decor"; end
+		local flg = true;
+		flg = flg and RDXUI.UFAnchorCheck(desc.anchor, state, errs);
+		flg = flg and RDXUI.UFFrameCheck_Proto("Frame_", desc, state, errs);
+		flg = flg and RDXUI.UFOwnerCheck(desc.owner, state, errs);
+		if flg then 
+			state:AddSlot("Frame_" .. desc.name);
+		end
+		return flg;
+	end;
+	ApplyFeature = function(desc, state)
+		local objname = "Frame_" .. desc.name;
+		local id = math.random(10000000);
+		local camera = "SetCameraZero";
+		if desc and desc.cameraType then camera = desc.cameraType; end
+		local unit = "player";
+		if desc and desc.unit then uid = desc.unit; end
+
+		-- Creation/destruction
+		local createCode = [[
+local _f = VFLUI.AcquireFrame("PlayerModel");
+VFLUI.StdSetParent(_f, ]] .. RDXUI.ResolveFrameReference(desc.owner) .. [[, ]] .. desc.flOffset .. [[);
+_f:SetPoint(]] .. RDXUI.AnchorCodeFromDescriptor(desc.anchor) .. [[);
+_f:SetWidth(]] .. desc.w .. [[); _f:SetHeight(]] .. desc.h .. [[);
+
+_f.rdxupdate =  function()
+	frame.]] .. objname .. [[:SetUnit("]] .. unit .. [[");
+	]].. camera ..[[(frame.]] .. objname .. [[);
+	if UnitIsVisible("]] .. unit .. [[") then 
+		frame.]] .. objname .. [[:Show();
+	else
+		frame.]] .. objname .. [[:Hide();
+	end
+end
+
+WoWEvents:Bind("UNIT_PORTRAIT_UPDATE", nil, function(uid) if uid == "]] .. unit .. [[" then _f.rdxupdate(), end, "]] .. id .. [[");
+--if "]] .. unit .. [[" == "target" then
+--	WoWEvents:Bind("PLAYER_TARGET_CHANGED", nil, _f.rdxupdate, "]] .. id .. [[");
+--end
+--if "]] .. unit .. [[" == "focus" then
+--	WoWEvents:Bind("PLAYER_FOCUS_CHANGED", nil, _f.rdxupdate, "]] .. id .. [[");
+--end
+frame.]] .. objname .. [[ = _f;
+]];
+		local destroyCode = [[
+WoWEvents:Unbind("]] .. id .. [[");
+frame.]] .. objname .. [[.rdxupdate = nil;
+frame.]] .. objname .. [[:Destroy(); frame.]] .. objname .. [[ = nil;
+]];
+		state:Attach(state:Slot("EmitCreate"), true, function(code) code:AppendCode(createCode); end);
+		state:Attach(state:Slot("EmitDestroy"), true, function(code) code:AppendCode(destroyCode); end);
+
+		return true;
+	end;
+	UIFromDescriptor = function(desc, parent, state)
+		local ui = VFLUI.CompoundFrame:new(parent);
+
+		local ed_name, ed_width, ed_height = RDXUI.GenNameWidthHeightPortion(ui, desc, state);
+
+		local owner = RDXUI.MakeSlotSelectorDropdown(ui, VFLI.i18n("Owner"), state, "Subframe_");
+		if desc and desc.owner then owner:SetSelection(desc.owner); end
+
+		local anchor = RDXUI.UnitFrameAnchorSelector:new(ui); anchor:Show();
+		anchor:SetAFArray(RDXUI.ComposeAnchorList(state));
+		if desc and desc.anchor then anchor:SetAnchorInfo(desc.anchor); end
+		ui:InsertFrame(anchor);
+		
+		local er = VFLUI.EmbedRight(ui, "Unit");
+		local dd_unit = VFLUI.Dropdown:new(er, unitSel);
+		dd_unit:SetWidth(100); dd_unit:Show();
+		if desc and desc.unit then dd_unit:SetSelection(desc.unit); end
+		er:EmbedChild(dd_unit); er:Show();
+		ui:InsertFrame(er);
+
+		local ed_flOffset = VFLUI.LabeledEdit:new(ui, 50); ed_flOffset:Show();
+		ed_flOffset:SetText(VFLI.i18n("FrameLevel offset"));
+		if desc and desc.flOffset then ed_flOffset.editBox:SetText(desc.flOffset); end
+		ui:InsertFrame(ed_flOffset);
+		
+		local er = VFLUI.EmbedRight(ui, VFLI.i18n("Camera Type"));
+		local dd_cameraType = VFLUI.Dropdown:new(er, _dd_cameratypes);
+		dd_cameraType:SetWidth(200); dd_cameraType:Show();
+		if desc and desc.cameraType then 
+			dd_cameraType:SetSelection(desc.cameraType); 
+		else
+			dd_cameraType:SetSelection("SetCameraZero");
+		end
+		er:EmbedChild(dd_cameraType); er:Show();
+		ui:InsertFrame(er);
+
+		function ui:GetDescriptor()
+			local a = ed_flOffset.editBox:GetNumber(); if not a then a=0; end a = VFL.clamp(a, -2, 5);
+			return { 
+				feature = "portrait_3d2"; version = 1;
+				name = ed_name.editBox:GetText();
+				w = ed_width:GetSelection();
+				h = ed_height:GetSelection();
+				owner = owner:GetSelection();
+				anchor = anchor:GetAnchorInfo();
+				unit = dd_unit:GetSelection();
+				flOffset = a;
+				cameraType = dd_cameraType:GetSelection();
+			};
+		end
+
+		return ui;
+	end;
+	CreateDescriptor = function()
+		return { 
+			feature = "portrait_3d2"; version = 1; 
+			name = "portrait3d"; owner = "decor";
+			w = 30; h = 30; 
+			anchor = {lp = "RIGHT", af = "Base", rp = "LEFT", dx = 0, dy = 0};
+			unit = "player";
+			flOffset = 1;
+		};
+	end;
+});
