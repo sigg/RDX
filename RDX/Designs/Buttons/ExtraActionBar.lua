a7d1ab6c89e0334526755561b36b2e63123925ce
sigg <sigg@wowrdx.com>
2011-12-10 12:24:37 +0100

    Add nex feature ExtraActionBar

---
+++ RDX/Designs/Buttons/ExtraActionBar.lua
@@ -0,0 +1,110 @@
+-- ExtraActionBar.lua
+-- OpenRDX
+-- Sigg Rashgarroth EU
+
+RDX.RegisterFeature({
+	name = "extraactionbar";
+	title = VFLI.i18n("Bar ExtraAction");
+	category = VFLI.i18n("Buttons");
+	test = true;
+	version = 1;
+	IsPossible = function(state)
+		if not state:Slot("DesignFrame") then return nil; end
+		if not state:Slot("Base") then return nil; end
+		return true;
+	end;
+	ExposeFeature = function(desc, state, errs)
+		if not RDXUI.DescriptorCheck(desc, state, errs) then return nil; end
+		desc.owner = "Base";
+		local flg = true;
+		flg = flg and RDXUI.UFFrameCheck_Proto("Frame_", desc, state, errs);
+		flg = flg and RDXUI.UFAnchorCheck(desc.anchor, state, errs);
+		if flg then state:AddSlot("Frame_" .. desc.name); end
+		return flg;
+	end;
+	ApplyFeature = function(desc, state)
+		local objname = "Bar_" .. desc.name;
+		local flo = tonumber(desc.flo); if not flo then flo = 5; end; flo = VFL.clamp(flo,1,10);
+		------------------ On frame creation
+		local createCode = [[
+local btn, btnOwner = nil, ]] .. RDXUI.ResolveFrameReference(desc.owner) .. [[;
+
+local h = VFLUI.AcquireFrame("BlizzardElement", "ExtraActionBarFrame");
+if h then
+	h.ignoreFramePositionManager = true;
+	VFLUI.StdSetParent(h, btnOwner);
+	h:SetFrameLevel(btnOwner:GetFrameLevel() + ]] .. flo .. [[);
+	h:SetPoint(]] .. RDXUI.AnchorCodeFromDescriptor(desc.anchor) .. [[);
+end
+
+frame.]] .. objname .. [[ = h;
+]];
+		state:Attach("EmitCreate", true, function(code) code:AppendCode(createCode); end);
+		
+		------------------ On frame destruction.
+		local destroyCode = [[
+if frame.]] .. objname .. [[ then 
+	frame.]] .. objname .. [[.ignoreFramePositionManager = true;
+	frame.]] .. objname .. [[:Destroy();
+	frame.]] .. objname .. [[ = nil;
+end
+
+]];
+		state:Attach(state:Slot("EmitDestroy"), true, function(code) code:AppendCode(destroyCode); end);
+
+		return true;
+	end;
+	UIFromDescriptor = function(desc, parent, state)
+		local ui = VFLUI.CompoundFrame:new(parent);
+		
+		------------- Core
+		ui:InsertFrame(VFLUI.Separator:new(ui, VFLI.i18n("Core Parameters")));
+
+		local ed_name = VFLUI.LabeledEdit:new(ui, 100); ed_name:Show();
+		ed_name:SetText(VFLI.i18n("Name"));
+		ed_name.editBox:SetText(desc.name);
+		ui:InsertFrame(ed_name);
+
+		
+		------------- Layout
+		ui:InsertFrame(VFLUI.Separator:new(ui, VFLI.i18n("Layout parameters")));
+		
+		--local owner = RDXUI.MakeSlotSelectorDropdown(ui, VFLI.i18n("Owner"), state, "Subframe_");
+		--if desc and desc.owner then owner:SetSelection(desc.owner); end
+		
+		local ed_flo = VFLUI.LabeledEdit:new(ui, 50); ed_flo:Show();
+		ed_flo:SetText(VFLI.i18n("FrameLevel offset"));
+		if desc and desc.flo then ed_flo.editBox:SetText(desc.flo); else ed_flo.editBox:SetText(5); end
+		ui:InsertFrame(ed_flo);
+
+		local anchor = RDXUI.UnitFrameAnchorSelector:new(ui); anchor:Show();
+		anchor:SetAFArray(RDXUI.ComposeAnchorList(state));
+		if desc and desc.anchor then anchor:SetAnchorInfo(desc.anchor); end
+		ui:InsertFrame(anchor);
+		
+		function ui:GetDescriptor()
+			if chk_bs:GetChecked() then chk_bkd:SetChecked(); end
+			return { 
+				feature = "extraactionbar"; version = 1;
+				name = ed_name.editBox:GetText();
+				-- layout
+				owner = "Base";
+				flo = VFL.clamp(ed_flo.editBox:GetNumber(), 1, 10);
+				anchor = anchor:GetAnchorInfo();
+			};
+		end
+
+		return ui;
+	end;
+	CreateDescriptor = function()
+		return { 
+			feature = "extraactionbar";
+			version = 1; 
+			name = "extraactionbar", 
+			owner = "Base";
+			anchor = { lp = "TOPLEFT", af = "Base", rp = "TOPLEFT", dx = 0, dy = 0};
+			flo = 5;
+		};
+	end;
+});
+
