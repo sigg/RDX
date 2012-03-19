638ee60d8cb9076c1ee2badc22b65600aeb15467
Sigg <sigg@wowrdx.com>
2012-03-19 11:51:14 +0100

    Texture icon work

--- RDX/Designs/Textures/PlayerStatusIcon.lua
+++ RDX/Designs/Textures/PlayerStatusIcon.lua
@@ -1,4 +1,28 @@
 
+RDX.RegisterTextureIcon({
+	name = "Player Status";
+	createCode = [[
+btn._t:SetTexture("Interface\\CharacterFrame\\UI-StateIcon");
+]];
+	paintCodeTest = [[
+	btn._t:SetTexCoord(0, 0.5, 0, 0.421875);
+	btn:Show();
+]];
+	paintCode = [[
+local name = UnitName(uid);
+local myunit = RDXDAL.GetMyUnit();
+if UnitAffectingCombat(uid) or ((myunit.rosterName == name) and IsResting()) then
+	btn._t:SetTexCoord(0, 0.5, 0, 0.421875);
+	if UnitAffectingCombat(uid) then btn._t:SetTexCoord(0.5, 1, 0, 0.5); end
+	if not btn:IsShown() then btn:Show(0.2); end
+else
+	if btn:IsShown() then btn:Hide(0.2); end
+end
+]]); 
+	event = "PLAYER_UPDATE_RESTING";
+});
+
+
 ----------------------------------------------------------------------
 -- An iconic representation of Unit status icon for player frame. (made by superraider)
 ----------------------------------------------------------------------
