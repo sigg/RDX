bc423a28c3abb0b5b17bacb2ce6be2eea272a896
sigg <sigg@wowrdx.com>
2013-03-02 16:24:25 +0100

    Fix bar states

--- RDX/Designs/Lists/ActionBarsHandler.lua
+++ RDX/Designs/Lists/ActionBarsHandler.lua
@@ -63,18 +63,19 @@ function __RDXGetStates(statestype)
 		str = string.format("[bar:2] 1; [bar:3] 2; [bar:4] 3; [bar:5] 4; [bar:6] 5; [bonusbar:5] 10; [vehicleui] %d; [possessbar] %d; [overridebar] %d;", GetVehicleBarIndex() - 1, GetVehicleBarIndex() - 1, GetOverrideBarIndex() - 1);
 		local class = myunit:GetClassMnemonic();
 		if class == "PRIEST" then str = str .. " [bonusbar:1] 6;";
-		elseif class == "ROGUE" then str = str .. " [bonusbar:1] 6; [bonusbar:2] 6;";
+		elseif class == "ROGUE" then str = str .. " [bonusbar:1] 6; [form:3] 6;";
 		elseif class == "DRUID" then str = str .. " [bonusbar:1,stealth] 5; [bonusbar:1] 6; [bonusbar:2] 7; [bonusbar:3] 8; [bonusbar:4] 9;";
-		elseif class == "WARRIOR" then str = str .. " [bonusbar:1] 6; [bonusbar:2] 7; [bonusbar:3] 8;";
-		elseif class == "WARLOCK" then str = str .. " [bonusbar:1] 6;";
+		elseif class == "WARRIOR" then str = str .. " [form:1] 6; [form:2] 7; [form:3] 8;";
+		elseif class == "WARLOCK" then str = str .. " [form:1] 6;";
 		elseif class == "MONK" then str = str .. " [bonusbar:1] 6; [bonusbar:2] 7; [bonusbar:3] 8;";
 		end
 	elseif statestype == "Stance" then
 		local class = myunit:GetClassMnemonic();
 		if class == "PRIEST" then str = str .. " [bonusbar:1] 6;";
-		elseif class == "ROGUE" then str = str .. " [bonusbar:1] 6; [bonusbar:2] 6;";
+		elseif class == "ROGUE" then str = str .. " [bonusbar:1] 6; [form:3] 6;";
 		elseif class == "DRUID" then str = str .. " [bonusbar:1,stealth] 5; [bonusbar:1] 6; [bonusbar:2] 7; [bonusbar:3] 8; [bonusbar:4] 9;";
-		elseif class == "WARRIOR" then str = str .. " [bonusbar:1] 6; [bonusbar:2] 7; [bonusbar:3] 8;";
+		elseif class == "WARRIOR" then str = str .. " [form:1] 6; [form:2] 7; [form:3] 8;";
+		elseif class == "WARLOCK" then str = str .. " [form:1] 6;";
 		elseif class == "MONK" then str = str .. " [bonusbar:1] 6; [bonusbar:2] 7; [bonusbar:3] 8;";
 		end
 	end
