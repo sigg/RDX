506a36561435b1a3793c63f74b8ff7d39a381752
sigg <sigg@wowrdx.com>
2014-11-01 09:49:08 +0100

    Remove DiskTheme

--- RDXDiskTheme/main.lua
+++
@@ -1,9 +0,0 @@
-﻿WoWEvents:Bind("VARIABLES_LOADED", nil, function()
-	if not RDXDiskTheme then RDXDiskTheme = {}; end
-	RDXDB.RegisterDisk("RDXDiskTheme", RDXDiskTheme);
-end);
-
---RDXEvents:Bind("INIT_DATABASE_LOADED", nil, function()
---	RDXDB.GetOrCreatePackage("RDXDiskTheme", "default", "1.0.0", "WoWRDX", "", "sigg@wowrdx.com", "http://www.wowrdx.com", "RDX package");
---	RDXDB.SetPackageMetadata("RDXDiskTheme", "default", "infoIsIndelible", true);
---end);
